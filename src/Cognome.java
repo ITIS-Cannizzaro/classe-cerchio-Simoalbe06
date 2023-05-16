4e48e5a42eb61578c65171ca81d899167d67199b
utente <utente@2q4za33by8nhu90mpolp>
2023-05-16 12:07:29 +0200

    classe cerchio

--- src/Cognome.java
+++ src/Cognome.java
@@ -1,43 +1,67 @@
-import java.util.Scanner;
 
-// Chiamare la classe col proprio cognome
-public class MainClass
-{
-	static Scanner in = new Scanner(System.in);
-	
-	public static void main(String[] args)
-	{
-		while(true)
-		{
-			stampaMenu();
-			int scelta = Integer.parseInt(in.nextLine());
-			switch(scelta)
-			{
-				//aggiungere, togliere casi a seconda delle proprie scelte
-				case 1:
-					//Inserire metodo statico
-				break;
-				case 2:
-					//Inserire metodo statico
-				break;
-				case 3:
-					//Inserire metodo statico
-				break;
-				default:
-					System.out.println("Scelta errata, riprova!");
-
-			}
-		}
+public class creazionec {
+	public static void main(String[] args) {
+		
+		metodi cerchio = new metodi(5.0);
+		metodi cerchioc = new metodi(cerchio);
+		System.out.println("diametro:" +cerchio.diametro());
+		System.out.println("circonferenza:" +cerchio.perif());
+		System.out.println("area:" +cerchio.area());
 	}
-	static void stampaMenu()
-	{
-		// Modificare il menù secondo le proprie scelte
-		System.out.println("1 - Es n. * - Titolo es. *");
-		System.out.println("2 - Es n. * - Titolo es. *");
-		System.out.println("3 - Es n. * - Titolo es. *");
-		System.out.println("4 - Es n. * - Titolo es. *");
-		System.out.println("5 - Es n. * - Titolo es. *");
-	}
-	
-	// Aggiungere qui i metodi statici relativi agli esercizi scelti
 }
+
+public class metodi {
+	double r;
+	double c;
+	public metodi(double r) {
+		this.r = r;
+	}
+	public double diametro() {
+		double diametro=r*2;
+		return diametro;
+	}
+	public double perif() {
+		double perif=r*2*3.14;
+			return perif;
+	}
+	public double area() {
+		double quadre=r*r;
+		double area = quadre*3.14;
+		return area;
+	}
+	public metodi(metodi c) {
+		this.r = c.getRaggio()*2;
+	}
+	public double getRaggio() {
+	return r;
+	}
+	@Override
+	public String toString() {
+		return "metodi [getRaggio()=" + getRaggio() + "]";
+	}
+	}
+
+
+
+package altronome;
+import java.util.Scanner;
+public class quattrodue {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("inserire il primo raggio");
+		int ru= scan.nextInt();
+		System.out.println("inserire il secondo raggio");
+		int rd= scan.nextInt();
+		quattro c1 = new quattro (ru);
+		quattro c2 = new quattro (rd);
+		System.out.println("dimetro primo cerchio :"+c1.diametro());
+		System.out.println("perimetro primo cerchio : "+c1.perimetro());
+		System.out.println("area primo cerchio : "+c1.area());
+		System.out.println("dimetro secondo cerchio :"+c2.diametro());
+		System.out.println("perimetro secondo cerchio : "+c2.perimetro());
+		System.out.println("area primo secondo : "+c2.area());
+	}
+
+}
+
+
